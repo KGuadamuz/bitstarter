2edf07a2a2b39c90ae48ede0eaf3450951928f68
KarenGuadamuz <karene.guadamuz@gmail.com>
2013-07-06 09:13:11 +0000

    Added code

--- web.js
+++ web.js
@@ -3,26 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(data));
+  
+fs.readFile('./index.hmtl', 'utf-8', function (error, data) {
 });
-
-fs.readFile('./index.hmtl', function read(err, data) {
-
-if (err) {
-
-throw err;
-}
-
-content = data;
-
-console.log(content);
-processFile();
-});
-
-function processFile() {
-sonsole.log(content);
-}
-
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
