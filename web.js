fad5efa359be4e9f4d75cfcb5ff3a5b0b0884a7c
KarenGuadamuz <karene.guadamuz@gmail.com>
2013-07-06 22:45:42 +0000

    Added code

--- web.js
+++ web.js
@@ -4,10 +4,9 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 
-response.send(data);
-
 var fs = require('fs');
 var data = fs.readFileSync("index.html", "utf8");
+response.send(data);
 
 
 });
