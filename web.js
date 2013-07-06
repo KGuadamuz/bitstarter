20ea64f37ceae729dc5603b340d14fc61a8856dd
KarenGuadamuz <karene.guadamuz@gmail.com>
2013-07-06 08:47:53 +0000

    Added code

--- web.js
+++ web.js
@@ -3,8 +3,16 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send = (fs.readSync(index.txt));
+  response.send());
 });
+
+var response.send = fs.readFile('./index.hmtl', function read(err, data) {
+
+if (err) {
+
+throw err;
+}
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
